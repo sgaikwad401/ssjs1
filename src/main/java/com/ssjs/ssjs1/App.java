19dc5c4a2c3b4a987b6d4e546c4f110a9f21c60f
sgaikwad <sgaikwad@gmail.com>
2021-04-17 19:52:59 +0530

    first commit

---
+++ src/main/java/com/ssjs/ssjs1/App.java
@@ -0,0 +1,13 @@
+package com.ssjs.ssjs1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello All, Welcome to Maven Session" );
+    }
+}
